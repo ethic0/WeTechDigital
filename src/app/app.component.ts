541858d9954015b2f2b93886cb000afe8917a00e
ethic0 <ethic0.aj@gmail.com>
2019-07-12 21:57:27 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import dummyData from '../assets/StudentData.json';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'WeTech Digital';
+  Data: any = dummyData;
+
+  //sorting
+  key: string = 'name';
+  reverse: boolean = false;
+  sort(key){
+    this.key = key;
+    this.reverse = !this.reverse;
+  }
+  
+}
